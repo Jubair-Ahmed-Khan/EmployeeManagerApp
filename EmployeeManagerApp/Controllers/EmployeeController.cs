aeee76766f478f648f2e6bf69cf888a969a30fc1
Jubair-Ahmed-Khan <jubairahmedkhantonmoy@gmail.com>
2024-09-15 01:16:28 +0600

    Add Employee Section added

--- EmployeeManagerApp/Controllers/EmployeeController.cs
+++ EmployeeManagerApp/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using EmployeeManagerApp.Services;
+﻿using EmployeeManagerApp.Models;
+using EmployeeManagerApp.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagerApp.Controllers
@@ -6,15 +7,64 @@ namespace EmployeeManagerApp.Controllers
     public class EmployeeController : Controller
     {
         private readonly EmployeeDbContext _context;
+        private readonly IWebHostEnvironment _environment;
 
-        public EmployeeController(EmployeeDbContext context)
+        public EmployeeController(EmployeeDbContext context,IWebHostEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
         public IActionResult Index()
         {
             var employees = _context.Employees.ToList();
             return View(employees);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeeDto employeeDto)
+        {
+            if (employeeDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "The image file is required");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return View(employeeDto); 
+            }
+
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(employeeDto.ImageFile!.FileName);
+
+            string imageFullPath = _environment.WebRootPath + "/profiles/" + newFileName;
+
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                employeeDto.ImageFile.CopyTo(stream);
+            }
+
+           
+
+            Employee employee = new Employee()
+            {
+                FullName = employeeDto.FullName,
+                Email= employeeDto.Email,
+                Mobile = employeeDto.Mobile,
+                DateOfBirth = employeeDto.DateOfBirth,
+                ImageFileName = newFileName
+            };
+
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Employee");
+
+            
         }
     }
 }
---
+++ EmployeeManagerApp/Models/EmployeeDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagerApp.Models
+{
+    public class EmployeeDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Mobile { get; set; }
+        [Required]
+        public string DateOfBirth { get; set; }
+        public IFormFile? ImageFile { get; set; }
+    }
+}
