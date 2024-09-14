53f2454e30e9c0099d7a4dcfed5d38ce1944213a
Jubair-Ahmed-Khan <jubairahmedkhantonmoy@gmail.com>
2024-09-14 23:11:42 +0600

    List of Employee Page Added

---
+++ EmployeeManagerApp/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagerApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagerApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeDbContext _context;
+
+        public EmployeeController(EmployeeDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var employees = _context.Employees.ToList();
+            return View(employees);
+        }
+    }
+}
