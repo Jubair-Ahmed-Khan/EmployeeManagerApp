4ea1e99110d8e198a3298e8ea2abd96c66ac2d55
Jubair-Ahmed-Khan <jubairahmedkhantonmoy@gmail.com>
2024-09-14 21:30:06 +0600

    Add project files.

---
+++ EmployeeManagerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeManagerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
