92816736963730443693af63f671c0964d2f1248
Jubair-Ahmed-Khan <jubairahmedkhantonmoy@gmail.com>
2024-09-15 02:32:04 +0600

    Edit Employee Section Added

--- EmployeeManagerApp/Controllers/EmployeeController.cs
+++ EmployeeManagerApp/Controllers/EmployeeController.cs
@@ -66,5 +66,76 @@ namespace EmployeeManagerApp.Controllers
 
             
         }
+
+        public IActionResult Edit(int id) 
+        {
+            var employee = _context.Employees.Find(id);
+
+            if(employee == null)
+            {
+                return RedirectToAction("Index", "Employee");
+            }
+
+            var employeeDto = new EmployeeDto()
+            {
+                FullName = employee.FullName,
+                Email = employee.Email,
+                Mobile = employee.Mobile,
+                DateOfBirth = employee.DateOfBirth
+            };
+
+            ViewData["EmployeeId"] = employee.Id;
+            ViewData["ImageFileName"] = employee.ImageFileName;
+
+            return View(employeeDto);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, EmployeeDto employeeDto)
+        {
+            var employee = _context.Employees.Find(id);
+
+            if(employee == null)
+            {
+                return RedirectToAction("Index", "Employee");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                ViewData["EmployeeId"] = employee.Id;
+                ViewData["ImageFileName"] = employee.ImageFileName;
+
+                return View(employeeDto);
+            }
+
+            string newFileName = employee.ImageFileName;
+
+            if(employeeDto.ImageFile != null)
+            {
+                newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                newFileName += Path.GetExtension(employeeDto.ImageFile.FileName);
+
+                string imageFullPath = _environment.WebRootPath + "/profiles/" + newFileName;
+
+                using (var stream = System.IO.File.Create(imageFullPath))
+                {
+                    employeeDto.ImageFile.CopyTo(stream);
+                }
+
+                string oldImageFullPath = _environment.WebRootPath + "/profiles/" + employee.ImageFileName;
+
+                System.IO.File.Delete(oldImageFullPath);
+            }
+
+            employee.FullName = employeeDto.FullName;
+            employee.Email = employeeDto.Email;
+            employee.Mobile = employeeDto.Mobile;
+            employee.DateOfBirth = employeeDto.DateOfBirth;
+            employee.ImageFileName = newFileName;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Employee");
+        }
     }
 }
